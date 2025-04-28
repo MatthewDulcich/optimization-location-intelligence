eaea5d2d7f92a046f24fd31faca8186d59bd6389
James Frech <jamesjfrech@gmail.com>
2025-04-27 20:58:33 -0400

    Added file with objective and constraints

--- optimizationFunction.py
+++ optimizationFunction.py
@@ -1,5 +1,4 @@
 
-
 def getConstraints(budget, N, risk):
     '''
     Creates constraints for optimization function given certain variables.
@@ -18,3 +17,68 @@ def getConstraints(budget, N, risk):
                   {'type': 'ineq', 'fun': lambda x: risk - cost/revenue}, # risk > cost/revenue
                   {'type': 'ineq', 'fun': lambda x: x} # All x >= 0
                  ]
+
+def demand(x,P,totalPop):
+    '''
+    Calculates demand as a function of price.
+
+    args:
+        x: Total number of stores in given county
+        P: Price
+        totalPop: Total population in county
+
+    returns:
+        demand: Demand corresponding to input variables.
+    '''
+
+    a = 0.003*P
+
+    # L: Maximum demand at unit price 1.0.
+    L = totalPop / x
+
+    demand = L*np.exp(-a*P)
+
+    return demand
+
+def revenue(demand,P,IR):
+    '''
+    Calculates Revenue of Product
+
+    args:
+        demand: The demand of a given product
+
+        P: Price of a given product
+
+        IR: Income ratio of county to maximum county income. Between 0 and 1.
+
+    returns:
+        revenue: Demand times Price
+    '''
+    return demand*P**IR
+
+def costs(x):
+    '''
+    SOMEONE FILL IN HERE!!!!!!!!!!!!!!!!!!!!!!!
+    '''
+    return(x)
+
+def profit(x, P, totalPop, IR):
+    '''
+    Calculates Profit
+
+    args:
+        x: Total number of stores in given county
+        P: Price
+        totalPop: Total population in county
+        IR: Income Ratio relative to maximum county income
+
+    '''
+    demand = demand(x,P,totalPop)
+
+    rev = revenue(demand,P,IR)
+    cost = costs(x)
+
+    return rev - cost
+
+def objectiveFunction(x, P, totalPop, IR):
+    return - x.T @ profit(x, P, totalPop, IR)
