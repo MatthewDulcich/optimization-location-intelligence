2ad2780f2d17bfe122af1fb9ccd5dd9c71a06ea3
James Frech <jamesjfrech@gmail.com>
2025-04-30 17:58:17 -0400

    Cleaned Constraints a bit

--- optimizationFunction.py
+++ optimizationFunction.py
@@ -1,23 +1,4 @@
 
-def getConstraints(budget, N, risk):
-    '''
-    Creates constraints for optimization function given certain variables.
-
-    Args:
-        budget (int): Total budget for owning restaurants.
-        N (int): Maximum number of stores to open.
-        risk (float): Total acceptable risk ratio per location (cost/revenue)
-
-    Returns:
-        contraints: List of contraints to use in optimization function.
-    '''
-    contraints = [
-                  {'type': 'ineq', 'fun': lambda x: budget - sum(costNet)}, # Budget >= x@costs
-                  {'type': 'ineq', 'fun': lambda x: N - sum(x)}, # N >= sum(x)
-                  {'type': 'ineq', 'fun': lambda x: risk - cost/revenue}, # risk > cost/revenue
-                  {'type': 'ineq', 'fun': lambda x: x} # All x >= 0
-                 ]
-
 def demand(x,P,totalPop):
     '''
     Calculates demand as a function of price.
@@ -59,7 +40,6 @@ def revenue(demand,P,IR):
 def costs(x):
     '''
     SOMEONE FILL IN HERE!!!!!!!!!!!!!!!!!!!!!!!
-    
     '''
     return(x)
 
@@ -77,9 +57,30 @@ def profit(x, P, totalPop, IR):
     demand = demand(x,P,totalPop)
 
     rev = revenue(demand,P,IR)
-    cost = costs(x)
+    cost = costs(x, P)
 
     return rev - cost
 
 def objectiveFunction(x, P, totalPop, IR):
     return - x.T @ profit(x, P, totalPop, IR)
+
+def getConstraints(budget, N, risk, x, P, demand, IR):
+    '''
+    Creates constraints for optimization function given certain variables.
+
+    Args:
+        budget (int): Total budget for owning restaurants.
+        N (int): Maximum number of stores to open.
+        risk (float): Total acceptable risk ratio per location (cost/revenue)
+
+    Returns:
+        contraints: List of contraints to use in optimization function.
+    '''
+    cost = costs(x, P)
+    revenue = revenue(demand, P, IR)
+    contraints = [
+                  {'type': 'ineq', 'fun': budget - sum(cost)}, # Budget >= x@costs
+                  {'type': 'ineq', 'fun': N - sum(x)}, # N >= sum(x)
+                  {'type': 'ineq', 'fun': risk - cost/revenue}, # risk > cost/revenue
+                  {'type': 'ineq', 'fun': x} # All x >= 0
+                 ]
