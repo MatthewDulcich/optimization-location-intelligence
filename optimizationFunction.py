64bfd118aba6d20313dcd6cab7834366bc1b9fe2
unknown <peeyu704@gmail.com>
2025-05-01 00:52:37 -0700

    First development of optimizationFunction.py

--- optimizationFunction.py
+++ optimizationFunction.py
@@ -1,5 +1,59 @@
+import numpy as np
 
-def demand(x,P,totalPop):
+
+def profit(x, P, totalPop, IR, minwage, rent, initialCosts):
+    '''
+    Calculates Profit = Revenue - Costs
+
+    args:
+        x: Total number of stores in given county
+        P: Price
+        totalPop: Total population in county
+        IR: Income Ratio relative to maximum county income
+        minwage: Minimum wage in county
+        rent: Rent of restaurant
+        initialCosts: Initial costs of opening restaurant
+
+    returns:
+        profit: Profit of restaurant
+    '''
+    demand_val = demand(x, P, totalPop)
+    rev = revenue(demand_val, P, IR)
+    cost = costs(minwage, rent, demand_val, P, initialCosts)
+    return rev - cost
+
+
+def revenue(demand, P, IR):
+    '''
+    Calculates Revenue of Product
+
+    args:
+        demand: The demand of a given product
+        P: Price of a given product
+        IR: Income ratio of county to maximum county income. Between 0 and 1.
+
+    returns:
+        revenue: Demand times Price
+    '''
+    return P**IR * demand
+
+
+def log_revenue(log_demand, P, IR):
+    '''
+    Calculates log of Revenue of Product. It is a concave function.
+
+    args:
+        demand: The demand of a given product
+        P: Price of a given product
+        IR: Income ratio of county to maximum county income. Between 0 and 1.
+
+    returns:
+        log_revenue: Log of Revenue
+    '''
+    return log_demand + IR * np.log(P)
+
+
+def demand(x, P, totalPop):
     '''
     Calculates demand as a function of price.
 
@@ -11,54 +65,135 @@ def demand(x,P,totalPop):
     returns:
         demand: Demand corresponding to input variables.
     '''
-
-    a = 0.003*P
-
+    a = 0.003 * P
     # L: Maximum demand at unit price 1.0.
     L = totalPop / x
+    return L * np.exp(-a * P)
 
-    demand = L*np.exp(-a*P)
 
-    return demand
-
-def revenue(demand,P,IR):
+def log_demand(x, P, totalPop):
     '''
-    Calculates Revenue of Product
+    Calculates log of demand as a function of price. It is a concave function.
 
     args:
-        demand: The demand of a given product
-
-        P: Price of a given product
-
-        IR: Income ratio of county to maximum county income. Between 0 and 1.
-
+        x: Total number of stores in given county
+        P: Price
+        totalPop: Total population in county
+    
     returns:
-        revenue: Demand times Price
+        log_demand: Log of demand corresponding to input variables.
     '''
-    return demand*P**IR
-# TODO: Integrate P into costs
-def costs(minwage, rent, loss, initialCosts, employees=15):
+    a = 0.003 * P
+    # L: Maximum demand at unit price 1.0.
+    L = totalPop / x
+    return np.log(L) - a * P
+
+
+def costs(minwage, rent, demand, P, initialCosts, employees=15):
     '''
     Calculates costs of restaurant.
+
     args:
         minwage: Minimum wage in county
         rent: Rent of restaurant
-        loss: Losses incurred by restaurant
+        demand: Demand of restaurant
+        P: Price of restaurant
+IR: Income ratio of county to maximum county income. Between 0 and 1.
         initialCosts: Initial costs of opening restaurant
         employees: Number of employees in restaurant (default is 15)
+    
     returns:
         netCosts: Total costs of restaurant
     '''
+    loss_incurred = loss(demand, P)
+    operating_costs = minwage * employees + rent + loss_incurred
+    return operating_costs + initialCosts
 
-    operatingCosts = minwage * employees + rent + loss
-    netCosts = operatingCosts + initialCosts
 
-
-    return(netCosts)
-
-def profit(x, P, totalPop, IR, minwage, rent, loss, initialCosts):
+def loss(demand, P, IR):
     '''
-    Calculates Profit
+    Calculates losses: 8.2% of revenue incurred by restaurant.
+
+    args:
+        demand: Demand of restaurant
+        IR: Income ratio of county to maximum county income. Between 0 and 1.
+        P: Price of restaurant
+
+    returns:
+        revenue(oss: Los, P, IR)
+    '''
+    return 0.082 * revenue(demand, P, IR)
+
+
+# ######################################################
+# Inequality constraints >= 0
+# ######################################################
+
+
+def max_risk(cost, revenue, risk=0.8):
+    '''
+    Calculates maximum risk of restaurant.
+
+    args:
+        cost: Total costs of restaurant
+        revenue: Total revenue of restaurant
+        risk: Acceptable risk threshold (default is 0.8)
+
+    returns:
+        maxRisk: Maximum risk of restaurant
+    '''
+    return risk - cost / revenue
+
+
+def store_possibility(x):
+    '''
+    Calculates if store is possible.
+
+    args:
+        x: Total number of stores in given county
+
+    returns:
+        storePossibility: True if store is possible, False otherwise
+    '''
+    return x
+
+
+def max_budget(budget, cost):
+    '''
+    Calculates maximum budget of restaurant.
+
+    args:
+        budget: Total budget of restaurant
+        cost: Total costs of restaurant
+
+    returns:
+        maxBudget: Maximum budget of restaurant
+    '''
+    return budget - sum(cost)
+
+
+def max_stores(N, x):
+    '''
+    Calculates maximum number of stores in given county.
+
+    args:
+        N: Maximum number of stores to open
+        x: Total number of stores in given county
+
+    returns:
+        maxStores: Remaining number of stores that can be opened
+    '''
+    return N - sum(x)
+
+
+# ######################################################
+# Objective function
+# ######################################################
+
+
+def objectiveFunction(x, P, totalPop, IR):
+    '''
+    Objective function for optimization problem.
 
     args:
         x: Total number of stores in given county
@@ -66,18 +201,13 @@ def profit(x, P, totalPop, IR, minwage, rent, loss, initialCosts):
         totalPop: Total population in county
         IR: Income Ratio relative to maximum county income
 
+    returns:
+        Objective value to minimize
     '''
-    demand = demand(x,P,totalPop)
+    return -x.T @ profit(x, P, totalPop, IR)
 
-    rev = revenue(demand,P,IR)
-    cost = costs(minwage, rent, loss, initialCosts, employees=15)
 
-    return rev - cost
-
-def objectiveFunction(x, P, totalPop, IR):
-    return - x.T @ profit(x, P, totalPop, IR)
-
-def getConstraints(budget, N, risk, x, P, demand, IR):
+def getConstraints(budget, N, risk, x, P, totalPop, IR):
     '''
     Creates constraints for optimization function given certain variables.
 
@@ -85,15 +215,20 @@ def getConstraints(budget, N, risk, x, P, demand, IR):
         budget (int): Total budget for owning restaurants.
         N (int): Maximum number of stores to open.
         risk (float): Total acceptable risk ratio per location (cost/revenue)
+        x: Total number of stores in given county
+        P: Price
+        totalPop: Total population in county
+        IR: Income Ratio relative to maximum county income
 
     Returns:
-        contraints: List of contraints to use in optimization function.
+        constraints: List of constraints to use in optimization function.
     '''
-    cost = costs(x, P)
-    revenue = revenue(demand, P, IR)
-    contraints = [
-                  {'type': 'ineq', 'fun': budget - sum(cost)}, # Budget >= x@costs
-                  {'type': 'ineq', 'fun': N - sum(x)}, # N >= sum(x)
-                  {'type': 'ineq', 'fun': risk - cost/revenue}, # risk > cost/revenue
-                  {'type': 'ineq', 'fun': x} # All x >= 0
-                 ]
+    demand_val = demand(x, P, totalPop)
+    cost = costs(minwage=0, rent=0, demand=demand_val, P=P, initialCosts=0)  # Replace with actual values
+    rev = revenue(demand_val, P, IR)
+    return [
+        {'type': 'ineq', 'fun': lambda _: budget - sum(cost)},  # Budget >= x@costs
+        {'type': 'ineq', 'fun': lambda _: N - sum(x)},  # N >= sum(x)
+        {'type': 'ineq', 'fun': lambda _: risk - cost / rev},  # risk > cost/revenue
+        {'type': 'ineq', 'fun': lambda _: x}  # All x >= 0
+    ]
